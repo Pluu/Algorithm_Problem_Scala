0956ecf09721aa56ff8af0770b28a39f4fb6e564
Pluu <pluulove@gmail.com>
2013-04-28 14:26:27 +0900

    Create Fibonacci_Even_Number_Sum.scala

---
+++ Fibonacci_Even_Number_Sum.scala
@@ -0,0 +1,10 @@
+/**
+ * Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million.*
+ */
+
+lazy val fs: Stream[Int] = 
+  0 #:: 1 #:: fs.zip(fs.tail).map(p => p._1 + p._2)
+
+val r = fs.view.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum
+
+assert(r == 4613732) // 1 ms
