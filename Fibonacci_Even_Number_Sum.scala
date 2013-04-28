f474b0c61430c1221e1fa29a521465843d94b835
Pluu <pluulove@gmail.com>
2013-04-28 14:29:15 +0900

    Update Fibonacci_Even_Number_Sum.scala

--- Fibonacci_Even_Number_Sum.scala
+++ Fibonacci_Even_Number_Sum.scala
@@ -1,4 +1,6 @@
 /**
+ * Original : http://pavelfatin.com/scala-for-project-euler/
+ * 
  * Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million.*
  */
 
